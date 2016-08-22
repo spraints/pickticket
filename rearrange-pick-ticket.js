94d71c73036e5be921914ccdcd3e352e6d7b4eb5
Matt Burke <spraints@gmail.com>
2016-08-22 09:45:41 -0400

    make a bookmarklet

---
+++ rearrange-pick-ticket.js
@@ -0,0 +1,39 @@
+//bookmarkify:
+//  $ npm install
+//  $ node_modules/.bin/uglify rearrange-pick-ticket.js | sed -e 's/^/javascript:/'
+(function() {
+  var mkarr = function(coll) { return Array.prototype.slice.call(coll) }
+  var groupBy = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
+
+  var strongs = mkarr(document.getElementsByTagName("strong"))
+  var customerStrongs = strongs.filter(function(e) { return "Customer:" == e.innerText })
+  var orders = customerStrongs.map(function(e) { return {cust: e} })
+  var orderContainer = undefined;
+  orders.forEach(function(order) {
+    var orderEl = order.cust.parentElement.parentElement
+    var custNumEl = order.cust.nextElementSibling
+    var orderStrongs = mkarr(orderEl.getElementsByTagName("strong"))
+    var productEl = orderStrongs.filter(function(e) { return "Product:" == e.innerText })[0]
+    if (orderContainer === undefined) orderContainer = orderEl.parentElement
+    custNumEl.style.cssText = "font-weight: bold; font-size: large; border: solid 1px black"
+    order.html = orderEl.outerHTML
+    order.custNum = parseInt(custNumEl.innerText)
+    order.product = productEl === undefined ? "unknown" : productEl.nextSibling.textContent
+    orderEl.remove()
+  })
+  var ordersByProduct = groupBy(orders, "product")
+  var newHtml = []
+  Object.keys(ordersByProduct).forEach(function(k) {
+    newHtml.push("<h2>" + k + "</h2>")
+    ordersByProduct[k].sort(function(a,b){return a.custNum - b.custNum}).forEach(function(order) {
+      newHtml.push(order.html)
+    })
+  })
+  orderContainer.innerHTML += newHtml.join("")
+//  window.mab = {obp: ordersByProduct, pel: orderContainer}
+})();
